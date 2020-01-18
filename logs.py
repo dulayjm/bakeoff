5c5eda6c783aa051a221d1b7ac2691936327e8e9
rolwesg <gregory.rolwes@slu.edu>
2020-01-17 22:07:50 -0600

    Delete logs.py

--- logs.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def newLogger(name):
-  logger = logging.getLogger(name)
-  formatter = logger.Formatter('%(asctime)s:%(levelname)s::  %(message)s')
-  file_handler = logging.FileHandler('models.log')
-  file_handler.setFormatter(formatter)
-  logger.addHandler(file_handler)

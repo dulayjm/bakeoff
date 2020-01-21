15cda17c3ec6e96f09a620adb88072593633e30d
Greg <gregory.rolwes@slu.edu>
2020-01-21 15:09:05 -0600

    removed top-X knn

--- accuracy/knearest.py
+++ accuracy/knearest.py
@@ -8,31 +8,26 @@ class KNN():
       test_data, test_labels = self.removeAtIndex([outputs.tolist(), labels], j)
       test_data = [torch.FloatTensor(embedding) for embedding in test_data]
       anchor_label = labels[j]
-      correct, _ = self.correct(anchor, anchor_label, torch.stack(test_data), test_labels)
+      correct = self.correct(anchor, anchor_label, torch.stack(test_data), test_labels)
       corrects += correct
       j += 1
     return corrects / j
 
-  def correct(self, anchor, anchor_label, weights, labels, top=5):
+  def correct(self, anchor, anchor_label, weights, labels):
     # error checking
     for w in weights:
       assert anchor is not w, "Test image should not be in set of images to get distance from"
 
-    # get X closest outputs to test image
+    # get closest outputs to test image
     dist = torch.norm(weights - anchor, dim=1, p=None)
-    knn = dist.topk(top, largest=False)
+    knn = dist.topk(1, largest=False)
 
-    # return if actual test image label is contained in the top X closest image labels
+    # return if actual test image label is contained in the closest image labels
     knn_labels = [labels[index] for index in knn.indices.tolist()]
-    # top match is the correct label
-    if (anchor_label == knn_labels[0]):
-      return [1, 1]
-    # correct label is in top 5
-    elif (anchor_label in knn_labels):
-      return [0, 1]
-    # no match
+    if (anchor_label in knn_labels):
+      return 1
     else:
-      return [0, 0]
+      return 0
 
   def removeAtIndex(self, lists, j):
     removed = []
--- main.py
+++ main.py
@@ -27,7 +27,7 @@ model_param = {
   "step_size": 7,
   "feature_extracting": False,
   "learning_rate": 0.001,
-  "name": "new_loader_debug2"
+  "name": "new_loader_debug2-delete"
 }
 
 logging.basicConfig(filename="{}.log".format(model_param["name"]), level=logging.DEBUG, format='%(asctime)s:%(levelname)s::  %(message)s')

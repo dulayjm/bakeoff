bc1132d6ee94e9fed7fa55f5e4d763b8b83659e1
Greg <gregory.rolwes@slu.edu>
2020-02-05 06:18:04 -0600

    Fully command line arguments

--- main.py
+++ main.py
@@ -16,12 +16,24 @@ import accuracy
 import numpy as np
 
 parser = argparse.ArgumentParser(description='PyTorch Training')
-parser.add_argument('-loss', default='triplet', required=True,
-                    help='path to dataset')
+parser.add_argument('-loss', default='batchall', required=False,
+                    help='loss function')
 parser.add_argument('-acc', default='knn', required=False,
-                    help='path to dataset')
+                    help='accuracy evaluation function')
+parser.add_argument('-epochs', default=50, required=False,
+                    help='number of epochs')
+parser.add_argument('-pretrain', default=False, required=False,
+                    help='pretrain on ImageNet?')
+parser.add_argument('-step_size', default=7, required=False,
+                    help='number of epochs between decreasing learning rate')
+parser.add_argument('-feature_extracting', default=False, required=False,
+                    help='should the model be feature extracting')
+parser.add_argument('-lr', default=0.001, required=False,
+                    help='optimizer learning rate')
+parser.add_argument('-output_layers', default=256, required=False,
+                    help='number of output layers')
 parser.add_argument('-name', default='model', required=True,
-                    help='path to dataset')
+                    help='custom model name')
 args = parser.parse_args()
 
 data = TEST()
@@ -33,12 +45,12 @@ model_param = {
   "loaders": {'train':train_loader, 'valid':valid_loader},
   "loss_fn": loss.create(args.loss),
   "acc_fn": accuracy.create(args.acc),
-  "epochs": 50,
-  "pretraining": False,
-  "step_size": 30,
-  "feature_extracting": False,
-  "learning_rate": 0.00001,
-  "output_layers": 256,
+  "epochs": int(args.epochs),
+  "pretraining": bool(args.pretrain),
+  "step_size": int(args.step_size),
+  "feature_extracting": bool(args.feature_extracting),
+  "learning_rate": int(args.lr),
+  "output_layers": int(args.output_layers),
   "name": args.name
 }
 

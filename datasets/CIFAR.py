e475b87a4d964482b7a3bbacf25ba602c89d7c9c
Greg Rolwes <gregory.rolwes@slu.edu>
2020-03-26 21:00:26 -0500

    changed image size

--- datasets/CIFAR.py
+++ datasets/CIFAR.py
@@ -3,6 +3,6 @@ from datasets.data import Data
 class CIFAR(Data):
     def __init__(self):
         CIFAR_dir = '/student/rolwesg/Data/CIFAR10/'
-        CIFAR_img_size = 32
+        CIFAR_img_size = 256
         self.name = "CIFAR"
         super().__init__(CIFAR_dir, CIFAR_img_size)
